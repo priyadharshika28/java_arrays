e13c8b730dd2b25a0ef0668bb8ca211e349957ad
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-09-22 14:50:07 +0530

    Create ascending.java

---
+++ ascending.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] res = ascending(arr);
+        for(int num:res){
+            System.out.print(num+" ");
+        }
+    }
+    public static int[] ascending(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
