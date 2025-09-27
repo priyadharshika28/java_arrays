e78b187a9bf2443d5dfc5430d21616d0ab25c58d
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-09-27 17:49:53 +0530

    Create unique elements_2.java

---
+++ unique elements_2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        uniqueelements(arr);
+    }
+    public static void uniqueelements(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count==1){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
