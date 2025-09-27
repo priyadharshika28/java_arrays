1f177b1758180c6a03098087d38e4f2b23c534c5
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-09-27 18:01:19 +0530

    Create second largest.java

---
+++ second largest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(secondlargest(arr));
+    }
+    public static int secondlargest(int[] arr){
+        int n  = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        int sl = arr[n-2];
+        return sl;
+    }
+}
