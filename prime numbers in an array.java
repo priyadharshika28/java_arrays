2f4ef180900901b2b6b9f572c12d24982dbd5f09
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-11-08 10:22:26 +0530

    Create prime numbers in an array.java

---
+++ prime numbers in an array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int res:arr){
+            if(isprime(res)){
+                System.out.print(res+" ");
+            }
+        }
+    }
+    public static boolean isprime(int n){
+        if(n<2) return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
